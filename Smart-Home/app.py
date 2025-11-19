1bb0579472f74150eb183ead4158f003569adaf0
MariVS0912 <mvargass3@eafit.edu.co>
2025-11-19 17:42:00 -0500

    Update app.py

--- Smart-Home/app.py
+++ Smart-Home/app.py
@@ -1,11 +1,10 @@
 import streamlit as st
 from mqtt_utils import connect_mqtt
 
-# Configuración de la página
 st.set_page_config(page_title="Smart-Home", page_icon="🏠")
 st.title("Smart-Home – Controla tu casa desde el celular")
 
 # Conectar al broker
-client = connect_mqtt(broker="TU_BROKER", port=8883, username="TU_USUARIO", password="TU_PASSWORD")
+connect_mqtt()
 
 st.write("Usa el menú lateral para navegar entre Dashboard y Controles.")
