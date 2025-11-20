98fd4d2d9fda65fa9c02192fef511832ea5c1403
MariVS0912 <mvargass3@eafit.edu.co>
2025-11-19 20:19:45 -0500

    Update app.py

--- Smart-Home/app.py
+++ Smart-Home/app.py
@@ -1,17 +1,19 @@
+# app.py
 import streamlit as st
 from mqtt_utils import connect_mqtt
 
 st.set_page_config(page_title="Smart-Home", page_icon="🏠")
 
-# Conectar MQTT solo una vez
-connect_mqtt()
+# Conexión MQTT
+connect_mqtt()  # Aquí se conecta al broker
 
 st.title("Smart-Home – Controla tu casa desde el celular")
 
-st.sidebar.title("Navegación")
+# Navegación
 page = st.sidebar.selectbox("Ir a:", ["Home Status", "Controles"])
 
 if page == "Home Status":
     import pages.home_status
 elif page == "Controles":
     import pages.controls
+
